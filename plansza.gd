19cc9c1213f7d920bee5a33f9b9497c28ae87931
bspat <154008056+bspat@users.noreply.github.com>
2024-01-19 19:27:50 +0100

    Nawiasy na pole()
    Zakończono wymianę nawiasów na funkcję pole (dla czytelności kodu)

--- plansza.gd
+++ plansza.gd
@@ -22,42 +22,42 @@ func initPlansza(eo:Array):
 	#pola startowe czarnych mają wartość 1
 	for i in 5:
 		eo[pole(4+i,1)]=1
-		eo[13*11+4+i]=1
-		eo[13*(i+4)+1]=1
-		eo[13*(i+4)+11]=1
-	eo[13*2+6]=1
-	eo[13*10+6]=1
-	eo[13*6+2]=1
-	eo[13*6+10]=1
+		eo[pole(4+i,11)]=1
+		eo[pole(1,(i+4))]=1
+		eo[pole(11,(i+4))]=1
+	eo[pole(6,2)]=1
+	eo[pole(6,10)]=1
+	eo[pole(2,6)]=1
+	eo[pole(10,6)]=1
 	#pola startowe białych pionów mają wartość 2
-	eo[13*6+4]=2
-	eo[13*6+5]=2
-	eo[13*6+7]=2
-	eo[13*6+8]=2
-	eo[13*4+6]=2
-	eo[13*8+6]=2
+	eo[pole(4,6)]=2
+	eo[pole(5,6)]=2
+	eo[pole(7,6)]=2
+	eo[pole(8,6)]=2
+	eo[pole(6,4)]=2
+	eo[pole(6,8)]=2
 	for i in 3:
-		eo[13*5+5+i]=2
-		eo[13*7+5+i]=2
+		eo[pole(5+i,5)]=2
+		eo[pole(5+i,7)]=2
 	#tron z królem ma wartość 7, potem sam król wartość 3, a tron i inne pola zastrzeżone 4
-	eo[13*6+6]=8
-	eo[13+1]=4
-	eo[13+11]=4
-	eo[13*11+1]=4
-	eo[13*11+11]=4
+	eo[pole(6,6)]=8
+	eo[pole(1,11)]=4
+	eo[pole(11,11)]=4
+	eo[pole(1,1)]=4
+	eo[pole(11,1)]=4
 
 func pokazPlansze(_eo:Array):
 	var napis
 	for i in 13:
 		napis = ""
 		for q in 13:
-			napis = napis + str(_eo[i*13+q]) + " "
+			napis = napis + str(_eo[pole(q,i)]) + " "
 		print(napis)
 
 func wolnePole(eo:Array, x:int,y:int):
-	if (eo[y*13+x]==0):
+	if (eo[pole(x,y)]==0):
 		return 1
-	elif (eo[y*13+x]==4):
+	elif (eo[pole(x,y)]==4):
 		return 2
 	else:
 		return 0	
@@ -68,21 +68,21 @@ func czyRuchLegalny(eo:Array, x1:int,y1:int,x2:int,y2:int):
 	if (x1==x2):
 		if y1<y2:
 			for i in range(y2,y1,-1):
-				if (wolnePole(eo,x1,i)==0)||(wolnePole(eo,x1,i)==2)&&((eo[13*y1+x1]==3)||(eo[13*y1+x1]==8)):
+				if (wolnePole(eo,x1,i)==0)||(wolnePole(eo,x1,i)==2)&&((eo[pole(x1,y1)]==3)||(eo[pole(x1,y1)]==8)):
 					return false
 		elif(y1>y2):
 			for i in range(y2,y1,1):
-				if (wolnePole(eo,x1,i)==0)||(wolnePole(eo,x1,i)==2)&&((eo[13*y1+x1]==3)||(eo[13*y1+x1]==8)):
+				if (wolnePole(eo,x1,i)==0)||(wolnePole(eo,x1,i)==2)&&((eo[pole(x1,y1)]==3)||(eo[pole(x1,y1)]==8)):
 					return false
 		else: return false	
 	elif (y1==y2):
 		if x1<x2:
 			for i in range(x2,x1,-1):
-				if (wolnePole(eo,i,y1)==0)||(wolnePole(eo,i,y1)==2)&&((eo[13*y1+x1]==3)||(eo[13*y1+x1]==8)):
+				if (wolnePole(eo,i,y1)==0)||(wolnePole(eo,i,y1)==2)&&((eo[pole(x1,y1)]==3)||(eo[pole(x1,y1)]==8)):
 					return false
 		elif(x1>x2):
 			for i in range(x2,x1,1):
-				if (wolnePole(eo,i,y1)==0)||(wolnePole(eo,i,y1)==2)&&((eo[13*y1+x1]==3)||(eo[13*y1+x1]==8)):
+				if (wolnePole(eo,i,y1)==0)||(wolnePole(eo,i,y1)==2)&&((eo[pole(x1,y1)]==3)||(eo[pole(x1,y1)]==8)):
 					return false
 	else: return false
 	return true
